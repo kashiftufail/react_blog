466c4c1735d049b69666fc9b0313f7dd27b2be04
kashif tufail <kashif.tufail17@gmail.com>
2022-09-01 18:10:26 +0500

    add route for create post and navigation

--- frontend/src/Posts/CreatePost.js
+++ frontend/src/Posts/CreatePost.js
@@ -1,52 +1,93 @@
 import react from "react";
 import useInput from "../useInput";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
+import "../App.css";
 
-import React from "react";
+import axios from 'axios';
+
 
 const CreatePost = () => {
 
+  const API_URL = "http://localhost:3002/api/v1/"
+  let navigate = useNavigate();
 
 
-    // const title = useInput("");
-    // const detail = useInput("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-    // const submitForm = (event) => {
-    //     event.preventDefault();
-    //     console.log("email", title.value);
-    //     console.log("password", detail.value);
-    // };
+  const onSubmit = (data) => {
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data.title);
-  
+    axios.post('http://localhost:3002/api/v1/posts', {
+      title: data.title,
+      content: data.content
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    navigate('/about');
 
 
+    // console.log(data);
+  }
 
   return (
-    // <div>CreatePost</div>
+
     <>
       <h2>New Post</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-    <div class="mb-3">
-        <label class="form-label" for="inputEmail">Title</label>
-        {/* <input type="text" class="form-control" id="inputEmail" placeholder="title" {...title}/> */}
-        <input class="form-control" {...register("title", { required: true, maxLength: 20 })} />
-
-    </div>
-    <div class="mb-3">
-        <label class="form-label" for="inputPassword">Detail</label>
-        <textarea rows="3" class="form-control" id="postalAddress" placeholder="deail" {...register("detail", { required: true, maxLength: 20 })}> </textarea>
-    </div>
-    {/* <div class="mb-3">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="checkRemember"/>
-            <label class="form-check-label" for="checkRemember">Remember me</label>
+        <div class="mb-3">
+          <label class="form-label" for="inputEmail">
+            Title
+          </label>
+        
+          <input
+            class="form-control"
+            {...register("title", {
+              required: true,
+              minLength: 5,
+              maxLength: 20,
+            })}
+          />
+          <p class="errors">
+            {errors.title?.type === "required" &&
+              "title is required.it should be atleast 5 and max 20 characters"}
+          </p>
         </div>
-    </div> */}
-    <button type="submit" class="btn btn-primary">Create</button>
-</form>
+        <div class="mb-3">
+          <label class="form-label" for="inputPassword">
+            Detail
+          </label>
+          <textarea
+            rows="3"
+            class="form-control"
+            placeholder="content"
+            {...register("content", {
+              required: true,
+              minLength: 20,
+              maxLength: 300,
+            })}
+          >
+            
+          </textarea>
+          <p class="errors">
+          {errors.content?.type === "required" &&
+            "content is required.it should be atleast 20 and max 20 characters"}
+          </p>  
+        </div>
+        
+        <button type="submit" class="btn btn-primary">
+          Create
+        </button>
+      </form>
     </>
   );
 };
